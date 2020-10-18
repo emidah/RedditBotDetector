1a039b68bdb5e3ff9d70bddcc59ea9ef752a0df2
Emil Dahlberg <emil.m.dahlberg@utu.fi>
2020-10-18 15:14:11 +0300

    Started work

---
+++ RedditBotDetector/CommentExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Reddit;
+using Reddit.Things;
+
+namespace RedditBotDetector {
+    public static class CommentExtensions {
+        public static IEnumerable<Comment> GetComments(this IEnumerable<CommentOrPost> corp) {
+            return corp.Where(item => item.Comment != null).Select(item => item.Comment);
+        }
+        public static Reddit.Controllers.Comment Load(this Comment comment, RedditClient client) {
+            return client.Comment(comment.Name).About();
+        }
+    }
+}
---
+++ RedditBotDetector/PostExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Reddit;
+using Reddit.Things;
+
+namespace RedditBotDetector {
+    public static class PostExtensions {
+        public static IEnumerable<Post> GetPosts(this IEnumerable<CommentOrPost> corp) {
+            return corp.Where(item => item.Post != null).Select(item => item.Post);
+        }
+
+        public static Reddit.Controllers.Post Load(this Post post, RedditClient client) {
+            return client.Post(post.Name).About();
+        }
+
+        public static IEnumerable<Reddit.Controllers.Post> Top(this IEnumerable<Reddit.Controllers.Post> posts, int count) {
+            return posts.OrderBy(post => post.UpVotes).Take(count);
+        }
+
+        public static IEnumerable<Reddit.Controllers.Comment> GetTopCommentsMany(this IEnumerable<Reddit.Controllers.Post> posts, int count) {
+            return posts.SelectMany(post => post.Comments.GetTop(5));
+        }
+
+        public static bool IsRepostOf(this Reddit.Controllers.Post self, Reddit.Controllers.Post other) {
+            return self.Created > other.Created && self.Title == other.Title && self.Subreddit == other.Subreddit;
+        }
+    }
+}
