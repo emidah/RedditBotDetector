f9f126977d58dfd209a1ea4a70859dc4756233cb
Emil Dahlberg <emil.m.dahlberg@utu.fi>
2020-10-19 22:59:54 +0300

    Refactor repost detection to own class

--- RedditBotDetector/Program.cs
+++ RedditBotDetector/Program.cs
@@ -1,15 +1,14 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
 using Reddit;
-using Reddit.Controllers;
 using RedditBotDetector.Extensions;
-using Comment = Reddit.Things.Comment;
 
 namespace RedditBotDetector {
     internal class Program {
+        private const string UserAgent = "dotnet:KarmaBotDetector:v0.1";
+
         private static int Main(string[] args) {
             var userName = args.Length != 1 ? string.Empty : args[0].Trim();
 
@@ -17,80 +16,47 @@ namespace RedditBotDetector {
                 return 1;
             }
 
-            Console.WriteLine($"Checking /u/{userName}");
-
             var services = ServiceProviderBuilder.GetServiceProvider(args);
             var secrets = services.GetRequiredService<IOptions<Secrets>>().Value;
-            var reddit = new RedditClient(secrets.AppId, appSecret: secrets.ClientSecret, refreshToken: secrets.RefreshToken, userAgent: "dotnet:KarmaBotDetector:v0.1");
+
+            var reddit = new RedditClient(secrets.AppId, appSecret: secrets.ClientSecret, refreshToken: secrets.RefreshToken,
+                userAgent: UserAgent);
+
+            Console.WriteLine($"Checking /u/{userName}");
+
             var user = reddit.User(userName);
             var postsOrComments = user.GetOverview(limit: 10).ToList();
             var totalPostsOrComments = postsOrComments.Count;
-
             if (totalPostsOrComments == 0) {
                 Console.WriteLine("This user has no posts or comments");
                 return 1;
             }
 
+            // Get which posts are reposts
             var posts = postsOrComments
                 .GetPosts()
-                .Select(post => reddit.Post(post.Name).About())
-                .OfType<LinkPost>()
                 .ToList();
             if (posts.Count > 0) {
                 // Check which of the user's posts are reposts
-                var reposts = posts.Select(post =>
-                        (post, reddit.Search(post.Title, limit: 25, sort: "num_comments")
-                            .FirstOrDefault(post.IsRepostOf)))
-                    .Where(tuple => tuple.Item2 != null)
-                    .ToList();
+                var reposts = RepostDetector.GetRepostsForPosts(reddit, posts);
                 Console.WriteLine($"Posts: {reposts.Count} out of {posts.Count} are reposts");
             } else {
                 Console.WriteLine("Posts: 0 posts in user's recent 10");
             }
 
-            // Get which comments are stolen from previous submissions of the parent posts:
+            // Get which comments are stolen from previous submissions of the parent posts
             var comments = postsOrComments
                 .GetComments()
                 .ToList();
 
             if (comments.Count > 0) {
-                // ReSharper disable RedundantEnumerableCastCall
-
-                // get posts related to comments
-                var commentsWithPosts = comments
-                    .Select(originalComment => (originalComment, reddit.GetPostForCommentListing(originalComment)))
-                    .Cast<(Comment originalComment, Post post)>();
-                // get duplicate posts, and from duplicate posts get some top comments
-                var commentsWithPostAndCommentsFromDuplicates = commentsWithPosts
-                    .Select(tuple => (tuple.originalComment, tuple.post,
-                        reddit.Search(NormalizeTitle(tuple.post.Title), limit: 25, sort: "num_comments")
-                            .Where(foundPost => foundPost.HasSameLink(tuple.post) && foundPost.Id != tuple.post.Id)
-                            .Top(5)
-                            .GetTopCommentsFlattened(50, 3)
-                            .ToList()))
-                    .Cast<(Comment originalComment, Post post, List<Comment> commentsFromDupes)>().ToList();
-
-                // Only keep comments where the duplicate post comments contain the original comment
-                var fakeCommentsWithPosts = commentsWithPostAndCommentsFromDuplicates
-                    .Where(tuple => tuple.commentsFromDupes.Any(comment => comment.Body == tuple.originalComment.Body))
-                    .Select(tuple => (tuple.originalComment, tuple.post))
-                    .ToList();
-
-                // ReSharper restore RedundantEnumerableCastCall
-
+                var fakeCommentsWithPosts = RepostDetector.GetRepostsForComments(comments, reddit);
                 Console.WriteLine($"Comments: {fakeCommentsWithPosts.Count} out of {comments.Count} are reposts");
             } else {
                 Console.WriteLine("Comments: 0 comments in user's recent 10");
             }
 
             return 0;
-        }
-
-        private static string NormalizeTitle(string postTitle) {
-            if (postTitle.EndsWith(".")) {
-                return postTitle.Substring(0, postTitle.Length - 1).Trim();
-            }
-            return postTitle;
         }
     }
 }
