86f542a94b3575d13a82af04dd6aee521471c276
Emil Dahlberg <emil.m.dahlberg@utu.fi>
2020-10-24 04:42:54 +0300

    Improve query

--- KarmaBotDetectorApi/ClientApp/src/app/query/query.component.ts
+++ KarmaBotDetectorApi/ClientApp/src/app/query/query.component.ts
@@ -21,19 +21,19 @@ export class FetchDataComponent {
     this.lastUsername = this.username;
     this.isFetching = true;
     this.http.get(this.baseUrl + "BotDetector/" + this.username).subscribe(result => {
+      this.isFetching = false;
       this.posts = result["posts"].map((rawPost: any): IPost => {
-        this.isFetching = false;
         const post: IPost = {
-          url: FetchDataComponent.redditUrl + rawPost.post.permalink,
-          originalUrl: FetchDataComponent.redditUrl + rawPost.originalPost.permalink
+          url: FetchDataComponent.redditUrl + rawPost.post?.permalink,
+          originalUrl: FetchDataComponent.redditUrl + rawPost.originalPost?.permalink
         }
         return post;
       });
       this.comments = result["comments"].map((rawPost: any): IPost => {
         const post: IComment = {
           url: FetchDataComponent.redditUrl + rawPost.comment?.permalink,
-          postUrl: FetchDataComponent.redditUrl + rawPost.commentPost.permalink,
-          originalUrl: FetchDataComponent.redditUrl + rawPost.originalComment.permalink
+          postUrl: FetchDataComponent.redditUrl + rawPost.commentPost?.permalink,
+          originalUrl: FetchDataComponent.redditUrl + rawPost.originalComment?.permalink
         }
         return post;
       });
